e7fcbaaa37c7f8c2129e7e59c9b43464c24d1afd
Huan Ming Liao <hmliao14@gmail.com>
2019-04-25 09:57:44 -0700

    initial import testing of navbar

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Navbar from '../components/Navbar';
 
 function App() {
   return (
     <div className="App">
       This is the app page
+      <Navbar/>
     </div>
   );
 }
