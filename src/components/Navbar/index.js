1e48b089991fbbe43850f6821bf43a62ba053644
Huan Ming Liao <hmliao14@gmail.com>
2019-04-25 09:57:25 -0700

    initial create a navbar and style sheet

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './style.styl'
+
+class Navbar extends Component{
+  render(){
+    return(
+      <div className="navbar">
+        this is a navbar
+      </div>
+    )
+  }
+}
+
+export default Navbar;
