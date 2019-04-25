3cd65c47eb7e366f447bf1c4a089b474b0dbdcc1
Huan Ming Liao <hmliao14@gmail.com>
2019-04-25 16:18:19 -0700

    turn app from function to class container

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Navbar from '../components/Navbar';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar/>
-      This is the app page
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <Navbar/>
+        This is the app page
+      </div>
+    );
+  }
 }
 
 export default App;
