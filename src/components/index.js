8ca4a653d891581f75f90bb68a56105f85a42f84
Huan Ming Liao <hmliao14@gmail.com>
2019-04-24 23:01:03 -0700

    moved app logic to containers and components

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import * as Icons from '@material-ui/icons/';
+import * as Charts from 'react-vis';
+
+// export all components, Icons, Charts
+export * from '@material-ui/core/';
+export { Icons, Charts };
