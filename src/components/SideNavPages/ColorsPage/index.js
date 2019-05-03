bc5acdfc5a19bf9aed89a1cda40e15fc5aaf70fa
Huan Ming Liao <hmliao14@gmail.com>
2019-05-03 16:42:58 -0700

    added intro headline for color page. Wrapped it with paper

--- src/components/SideNavPages/ColorsPage/index.js
+++ src/components/SideNavPages/ColorsPage/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './style.styl';
-import {AppBar} from '../../';
+import {Paper} from '../../';
 
 function ColorsPage(props){
   return(
     <div className="color-page">
-      <h1>Colormind.io</h1>
-      <iframe src="http://colormind.io/"> </iframe>
+      <Paper className="paper">
+        <h2>This page features an external website - Colormind.io using iframe</h2>
+        <br/>
+        <iframe src="http://colormind.io/"> </iframe>
+      </Paper>
     </div>
   )
 
