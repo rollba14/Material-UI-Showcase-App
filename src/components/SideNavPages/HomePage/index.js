344094d8e9509b26caea3158e4ab9337cdac8178
Huan Ming Liao <hmliao14@gmail.com>
2019-05-03 16:36:03 -0700

    Update home page with an intro about this app

--- src/components/SideNavPages/HomePage/index.js
+++ src/components/SideNavPages/HomePage/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import './style.styl';
-import {AppBar} from '../../';
+import {Paper} from '../../';
 
 function HomePage(props){
   return(
     <div className="home-page">
-      This is the home page
+      <div className="content">
+        <Paper className="paper">
+          <h1>Hi! Welcome to my Material UI Showcase App!</h1>
+          <br/>
+          <p>
+            This is an app about all the modules and components I can build with 3rd party library Material UI, to create an elegant, easy-to-use web user interface (UI) while keeping the UI responsive and compatible for different browsers and devices.
+          </p>
+          <br/>
+          <p>
+            To learn more, simply click on any nav link in the left side bar.
+          </p>
+        </Paper>
+      </div>
     </div>
   )
 
