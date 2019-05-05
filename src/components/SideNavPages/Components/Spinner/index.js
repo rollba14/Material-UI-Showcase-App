09911bac421c3a24ffbe339fa1ad452be3b3491b
Huan Ming Liao <hmliao14@gmail.com>
2019-05-04 22:39:26 -0700

    added spinner page

--- src/components/SideNavPages/Components/Spinner/index.js
+++ src/components/SideNavPages/Components/Spinner/index.js
@@ -3,10 +3,19 @@ import './style.styl';
 import {CircularProgress} from '../../../';
 import TabsTemplate from '../../../TabsTemplate'
 
+function createContent(props){
+  return (<div className="flex-around-center-wrap">
+    <CircularProgress color="primary"/>
+    <CircularProgress color="secondary" size={50} thickness={4}/>
+    <CircularProgress color="primary" size={60} thickness={4.4}/>
+    <CircularProgress color="secondary" size={70} thickness={4.8}/>
+  </div>)
+}
+
 function SpinnerPage(props){
   return(
     <div>
-      <TabsTemplate label="Spinner"/>
+      <TabsTemplate label="Spinner" content={createContent()}/>
     </div>
   )
 
