26571b6a6d24d320906723b62e4bb03179c5f123
Huan Ming Liao <hmliao14@gmail.com>
2019-05-05 02:25:54 -0700

    created notification page

--- src/components/SideNavPages/Components/Notification/index.js
+++ src/components/SideNavPages/Components/Notification/index.js
@@ -1,15 +1,62 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './style.styl';
-import { Button, Snackbar, MessageBar } from '../../../';
+import { Button, Snackbar, Icons, IconButton } from '../../../';
 import TabsTemplate from '../../../TabsTemplate'
 
-function NotificationPage(props){
-  return(
-    <div>
-      <TabsTemplate label="Notification"/>
-    </div>
-  )
+class NotificationPage extends Component{
+  constructor(props){
+    super(props)
+    this.state ={
+      open: false
+    }
+  }
 
+  handleOpen=()=>{
+    this.setState({
+      open: true
+    })
+  }
+
+  handleClose=(event,reason)=>{
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    this.setState({ open: false });
+  }
+
+  render(){
+    const content = (<div className="demo-notification-wrapper">
+      <Button color="primary" variant="contained" onClick={this.handleOpen}>
+        Trigger a notification
+      </Button>
+      <Snackbar
+        id="demo-notification"
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "right"
+        }}
+        open={this.state.open}
+        autoHideDuration={5000}
+        onClose={this.handleClose}
+        message={
+          <span>A new notification has triggered!</span>
+        }
+        action={
+          <IconButton
+           onClick={this.handleClose}>
+           <Icons.Close/>
+          </IconButton>
+        }
+      />
+    </div>)
+
+    return(
+      <div>
+        <TabsTemplate label="Notification" content={content}/>
+      </div>
+    )
+  }
 }
 
 export default NotificationPage;
