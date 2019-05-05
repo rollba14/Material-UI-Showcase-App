63966fb50209e17def6a2a855a93b7dc0ec50887
Huan Ming Liao <hmliao14@gmail.com>
2019-05-04 23:28:37 -0700

    added linear progress bars

--- src/components/SideNavPages/Components/Progress/index.js
+++ src/components/SideNavPages/Components/Progress/index.js
@@ -3,10 +3,17 @@ import './style.styl';
 import {LinearProgress} from '../../../';
 import TabsTemplate from '../../../TabsTemplate'
 
+function createContent(props){
+  return (<div className="demo-progress">
+    <LinearProgress color="primary"/>
+    <LinearProgress color="secondary" variant="query"/>
+  </div>)
+}
+
 function ProgressPage(props){
   return(
     <div>
-      <TabsTemplate label="Progress"/>
+      <TabsTemplate label="Progress" content={createContent()}/>
     </div>
   )
 
