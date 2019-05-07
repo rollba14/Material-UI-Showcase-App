0da8b6077f9f6ac0446e6c75555ddfc0f1c67518
Huan Ming Liao <hmliao14@gmail.com>
2019-05-07 16:54:36 -0700

    added messagebar markdown page

--- src/components/SideNavPages/Components/MessageBar/index.js
+++ src/components/SideNavPages/Components/MessageBar/index.js
@@ -1,57 +1,79 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './style.styl';
-import {SnackbarContent, Icons, IconButton} from '../../../';
+import {SnackbarContent, Icons, IconButton, CodeBlock, ReactMarkdown} from '../../../';
 import TabsTemplate from '../../../TabsTemplate'
+import markdownFile from './markdown.md'
 
-function createContent(props){
-  let messageBarsType = [
-    {
-      "success": "CheckCircle",
-    },
-    {
-      "warning": "Warning",
-    },
-    {
-      "error": "Error",
-    },
-    {
-      "info": "Info",
-    }
-  ]
+class MessageBarPage extends Component{
+  constructor(props) {
+    super(props)
+    this.state = { code: null }
+  }
 
-  return (messageBarsType.map(t=>{
-    let Icon= Icons[Object.values(t)[0]]
-    let msgType = Object.keys(t)[0];
-    let message = msgType.charAt(0).toLowerCase() === ('a' || 'e' || 'i' || 'o' || 'u') ? `This is an ${msgType} message!` : ` This is a ${msgType} message!`;
+  componentWillMount() {
+    fetch(markdownFile).then((response) => response.text()).then((text) => {
+      this.setState({ code: text })
+    })
+  }
 
-    return (<div className="messageBar" key={msgType+'-message'} >
-      <SnackbarContent
-       className={msgType+'-message'}
-       message={
-         <span >
-          <Icon/>
-          <div className="message">{message}</div>
-         </span>
-       }
-       action={[
-         <IconButton
-         >
-          <Icons.Close/>
-         </IconButton>
-       ]}
-       />
-    </div>)
-  }))
+  buildMarkdown=()=>{
+    return(<ReactMarkdown
+      source={this.state.code}
+      renderers={{code:CodeBlock}}
+    />)
+  }
 
-}
+  createContent=()=>{
+    let messageBarsType = [
+      {
+        "success": "CheckCircle",
+      },
+      {
+        "warning": "Warning",
+      },
+      {
+        "error": "Error",
+      },
+      {
+        "info": "Info",
+      }
+    ]
 
-function MessageBarPage(props){
-  return(
-    <div>
-      <TabsTemplate label="Message Bar" content={createContent()}/>
-    </div>
-  )
+    return (messageBarsType.map(t=>{
+      let Icon= Icons[Object.values(t)[0]]
+      let msgType = Object.keys(t)[0];
+      let message = msgType.charAt(0).toLowerCase() === ('a' || 'e' || 'i' || 'o' || 'u') ? `This is an ${msgType} message!` : ` This is a ${msgType} message!`;
 
+      return (<div className="messageBar" key={msgType+'-message'} >
+        <SnackbarContent
+         className={msgType+'-message'}
+         message={
+           <span >
+            <Icon/>
+            <div className="message">{message}</div>
+           </span>
+         }
+         action={[
+           <IconButton
+           >
+            <Icons.Close/>
+           </IconButton>
+         ]}
+         />
+      </div>)
+    }))
+  }
+
+  render(){
+    return(
+      <div>
+        <TabsTemplate label="Message Bar"
+          content={this.createContent()}
+          markdown={this.buildMarkdown()}
+        />
+      </div>
+    )
+  }
 }
 
 export default MessageBarPage;
