8ec5aacf5cbc66dc51b94463736bcd36fff00d85
Huan Ming Liao <hmliao14@gmail.com>
2019-05-06 17:55:33 -0700

    change tab's page to class comp. Updated content

--- src/components/SideNavPages/Components/Tabs/index.js
+++ src/components/SideNavPages/Components/Tabs/index.js
@@ -1,15 +1,49 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './style.styl';
-import {AppBar,Tabs,Tab} from '../../../';
+import {AppBar,Tabs,Tab, Paper} from '../../../';
 import TabsTemplate from '../../../TabsTemplate'
 
-function TabsPage(props){
-  return(
-    <div>
-      <TabsTemplate label="Tabs" content={<TabsTemplate label="Sample" content={"Contents of Tab 1"} markdown={"Contents of Tab 2"}/>}/>
-    </div>
-  )
+class TabsPage extends Component{
+  constructor(props){
+    super(props)
+    this.state={
+      selectedTab: 0
+    }
+  }
 
+  handleTabChange=(event,value)=>{
+    this.setState({
+      selectedTab: value
+    })
+  }
+
+  render(){
+    const value = this.state.selectedTab;
+    const content =(
+      <div>
+        <Paper>
+          <AppBar className="app-bar" position="static">
+            <Tabs value={value} onChange={this.handleTabChange}
+            indicatorColor="primary"
+            textColor="primary"
+            >
+              <Tab label="Tab 1" className="tab"/>
+              <Tab label="Tab 2" className="tab"/>
+            </Tabs>
+          </AppBar>
+          <div className="example-body">
+            {value === 0 && <div>Content 1</div>}
+            {value === 1 && <div>Content 2</div>}
+          </div>
+        </Paper>
+      </div>
+    )
+    return(
+      <div>
+        <TabsTemplate label="Tabs" content={content}/>
+      </div>
+    )
+  }
 }
 
 export default TabsPage;
