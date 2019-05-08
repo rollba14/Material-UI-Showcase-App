41c7cb4ff2b905f27bde8452cb749cd0950a4990
Huan Ming Liao <hmliao14@gmail.com>
2019-05-07 17:31:14 -0700

    added spinner markdown page

--- src/components/SideNavPages/Components/Spinner/index.js
+++ src/components/SideNavPages/Components/Spinner/index.js
@@ -1,24 +1,47 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './style.styl';
-import {CircularProgress} from '../../../';
+import {CircularProgress, CodeBlock, ReactMarkdown } from '../../../';
 import TabsTemplate from '../../../TabsTemplate'
+import markdownFile from './markdown.md'
 
-function createContent(props){
-  return (<div>
-    <CircularProgress color="primary"/>
-    <CircularProgress color="secondary" size={50} thickness={4}/>
-    <CircularProgress color="primary" size={60} thickness={4.4}/>
-    <CircularProgress color="secondary" size={70} thickness={4.8}/>
-  </div>)
-}
+class SpinnerPage extends Component{
+  constructor(props) {
+    super(props)
+    this.state = { code: null }
+  }
 
-function SpinnerPage(props){
-  return(
-    <div className="demo-spinner">
-      <TabsTemplate label="Spinner" content={createContent()}/>
-    </div>
-  )
+  componentWillMount() {
+    fetch(markdownFile).then((response) => response.text()).then((text) => {
+      this.setState({ code: text })
+    })
+  }
 
+  buildMarkdown=()=>{
+    return(<ReactMarkdown
+      source={this.state.code}
+      renderers={{code:CodeBlock}}
+    />)
+  }
+
+  createContent=()=>{
+    return (<div>
+      <CircularProgress color="primary"/>
+      <CircularProgress color="secondary" size={50} thickness={4}/>
+      <CircularProgress color="primary" size={60} thickness={4.4}/>
+      <CircularProgress color="secondary" size={70} thickness={4.8}/>
+    </div>)
+  }
+
+  render(){
+    return(
+      <div className="demo-spinner">
+        <TabsTemplate label="Spinner"
+          content={this.createContent()}
+          markdown={this.buildMarkdown()}
+        />
+      </div>
+    )
+  }
 }
 
 export default SpinnerPage;
