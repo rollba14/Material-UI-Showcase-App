6eab819db17709c48bcf8fe7a418e49e15d9074b
Huan Ming Liao <hmliao14@gmail.com>
2019-05-07 17:24:58 -0700

    added progress markdown page

--- src/components/SideNavPages/Components/Progress/index.js
+++ src/components/SideNavPages/Components/Progress/index.js
@@ -1,22 +1,45 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './style.styl';
-import {LinearProgress} from '../../../';
+import {LinearProgress, CodeBlock, ReactMarkdown} from '../../../';
 import TabsTemplate from '../../../TabsTemplate'
+import markdownFile from './markdown.md'
 
-function createContent(props){
-  return (<div>
-    <LinearProgress color="primary"/>
-    <LinearProgress color="secondary" variant="query"/>
-  </div>)
-}
+class ProgressPage extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { code: null }
+  }
 
-function ProgressPage(props){
-  return(
-    <div className="demo-progress">
-      <TabsTemplate label="Progress" content={createContent()}/>
-    </div>
-  )
+  componentWillMount() {
+    fetch(markdownFile).then((response) => response.text()).then((text) => {
+      this.setState({ code: text })
+    })
+  }
 
+  buildMarkdown=()=>{
+    return(<ReactMarkdown
+      source={this.state.code}
+      renderers={{code:CodeBlock}}
+    />)
+  }
+
+  createContent=()=>{
+    return (<div>
+      <LinearProgress color="primary"/>
+      <LinearProgress color="secondary" variant="query"/>
+    </div>)
+  }
+
+  render(){
+    return(
+      <div className="demo-progress">
+        <TabsTemplate label="Progress"
+          content={this.createContent()}
+          markdown={this.buildMarkdown()}
+        />
+      </div>
+    )
+  }
 }
 
 export default ProgressPage;
---
+++ src/components/SideNavPages/Components/Progress/markdown.md
@@ -0,0 +1,8 @@
+```js
+import LinearProgress from '@material-ui/core/LinearProgress';
+
+<LinearProgress
+  color="primary | secondary"
+  variant="determinate | indeterminate | buffer | query"
+/>
+```
