76a60e0d7d6ecd35dddbee9d9fc585b40949937d
Huan Ming Liao <hmliao14@gmail.com>
2019-05-07 17:57:34 -0700

    added table markdown page

--- src/components/SideNavPages/Components/Table/index.js
+++ src/components/SideNavPages/Components/Table/index.js
@@ -1,58 +1,81 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './style.styl';
-import {Paper, Table, TableBody, TableCell, TableHead, TableRow} from '../../../';
+import {Paper, Table, TableBody, TableCell, TableHead, TableRow, CodeBlock, ReactMarkdown} from '../../../';
 import TabsTemplate from '../../../TabsTemplate'
+import markdownFile from './markdown.md'
 
-function createContent(props){
-  
-  function createData(name, calories, fat, carbs, protein){
+class TablePage extends Component{
+  constructor(props) {
+    super(props)
+    this.state = { code: null }
+  }
+
+  componentWillMount() {
+    fetch(markdownFile).then((response) => response.text()).then((text) => {
+      this.setState({ code: text })
+    })
+  }
+
+  buildMarkdown=()=>{
+    return(<ReactMarkdown
+      source={this.state.code}
+      renderers={{code:CodeBlock}}
+    />)
+  }
+
+  createData=(name, calories, fat, carbs, protein)=>{
     return {name, calories, fat, carbs, protein };
   }
 
-  const data = [
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData('Ice cream sandwich', 237, 9.0, 3, 4.3),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
-  ];
+  createContent=()=>{
+    const data = [
+      this.createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+      this.createData('Ice cream sandwich', 237, 9.0, 3, 4.3),
+      this.createData('Eclair', 262, 16.0, 24, 6.0),
+      this.createData('Cupcake', 305, 3.7, 67, 4.3),
+      this.createData('Gingerbread', 356, 16.0, 49, 3.9),
+    ];
 
-  return (
-    <Paper className="demo-table">
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat (g)</TableCell>
-            <TableCell align="right">Carbs (g)</TableCell>
-            <TableCell align="right">Protein (g)</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {data.map(row => (
-            <TableRow key={row.name}>
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
+    return (
+      <Paper className="demo-table">
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Dessert (100g serving)</TableCell>
+              <TableCell align="right">Calories</TableCell>
+              <TableCell align="right">Fat (g)</TableCell>
+              <TableCell align="right">Carbs (g)</TableCell>
+              <TableCell align="right">Protein (g)</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </Paper>
-  );
-}
+          </TableHead>
+          <TableBody>
+            {data.map(row => (
+              <TableRow key={row.name}>
+                <TableCell component="th" scope="row">
+                  {row.name}
+                </TableCell>
+                <TableCell align="right">{row.calories}</TableCell>
+                <TableCell align="right">{row.fat}</TableCell>
+                <TableCell align="right">{row.carbs}</TableCell>
+                <TableCell align="right">{row.protein}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </Paper>
+    );
+  }
 
-function TablePage(props){
-  return(
-    <div className='button-ex'>
-      <TabsTemplate label="Table" content={createContent()}/>
-    </div>
-  )
+  render(){
+    return(
+      <div className='button-ex'>
+        <TabsTemplate label="Table"
+        content={this.createContent()}
+        markdown={this.buildMarkdown()}
+        />
+      </div>
+    )
+  }
 }
 
 export default TablePage;
