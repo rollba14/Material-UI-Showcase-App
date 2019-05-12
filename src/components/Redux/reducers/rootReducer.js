75416b41835d558570963d5275d200775555a5fc
Huan Ming Liao <hmliao14@gmail.com>
2019-05-11 23:59:23 -0700

    added toggle_sidebar action type to root reducer

--- src/components/Redux/reducers/rootReducer.js
+++ src/components/Redux/reducers/rootReducer.js
@@ -4,7 +4,15 @@ const rootReducer = (
   },
   action
 )=>{
-  return state;
+  switch(action.type){
+    case 'TOGGLE_SIDEBAR':
+      return {
+        ...state,
+        toggleSidebar: !state.toggleSidebar,
+      }
+    default:
+      return state;
+  }
 }
 
 export default rootReducer;
