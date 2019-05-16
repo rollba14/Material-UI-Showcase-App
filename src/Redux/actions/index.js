6461447ef0e478bad71bbf53cc1f191b2992b8c9
Huan Ming Liao <hmliao14@gmail.com>
2019-05-16 13:58:22 -0700

    added actionType to action folder. Added export to individual file, not export default {}

--- src/Redux/actions/index.js
+++ src/Redux/actions/index.js
@@ -4,34 +4,30 @@ export const ActionType = {
   OPEN_RIGHT_ITEM_MENU: "OPEN_RIGHT_ITEM_MENU",
   CLOSE_RIGHT_ITEM_MENU:
   "CLOSE_RIGHT_ITEM_MENU",
-}
+};
 
-const toggleCollapsableItem = (newLabel)=>{
+export const toggleCollapsableItem = (newLabel)=>{
   return{
     type: ActionType.TOGGLE_COLLAPSABLE_ITEM,
     label: newLabel,
   }
 };
 
-const toggleSidebar = ()=>{
+export const toggleSidebar = ()=>{
   return{
     type: ActionType.TOGGLE_SIDEBAR,
   }
 };
 
-const openRightItemMenu = (event)=>{
+export const openRightItemMenu = (event)=>{
   return {
     type: ActionType.OPEN_RIGHT_ITEM_MENU,
     anchorEl: event.currentTarget,
   }
 };
 
-const closeRightItemMenu = () =>{
+export const closeRightItemMenu = () =>{
   return {
     type: ActionType.CLOSE_RIGHT_ITEM_MENU,
   }
-}
-
-export default {
-  toggleCollapsableItem, toggleSidebar, openRightItemMenu, closeRightItemMenu,
-}
+};
