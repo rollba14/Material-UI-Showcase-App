7821d523baf1afdab25ed1a4d8fd459c9dc6c69b
Huan Ming Liao <hmliao14@gmail.com>
2019-05-29 21:50:47 -0700

    updated home page text and footer text

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,7 +5,7 @@ import {AppBar, Toolbar, Drawer, Icons, IconButton, Button, withStyles, Typograp
 function Footer(props){
   return(
     <div className='footer'>
-      © 2019 Huan Ming Liao
+      © 2019
     </div>
   )
 
